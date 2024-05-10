32a95bbf562bd108754a52087655dd9ef6f042a5
Bryan <bsanabriam@ucenfotec.ac.cr>
2024-05-10 11:51:56 -0600

    LandingPage

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <main className="container mx-auto main__container">
+            <section className="flex flex-col">
+                <div className='flex flex-row'>
+                    <h1>
+                        Humanidad101
+                    </h1>
+                    <div className="container">
+                        <div className="flex flex-row">
+                            <div>Logo</div>
+                            <div>Logo</div>
+                            <div>Logo</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container mx-auto">
+
+                </div>
+                <div class="container mx-auto">
+
+                </div>
+            </section>
+        </main>
+    );
+}
+
+export default LandingPage;
